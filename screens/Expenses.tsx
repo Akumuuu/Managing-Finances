f3102c8c36832b669bd68d1f12dd81d030f1d633
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2024-03-10 17:28:25 +0100

    skeleton + settings screen (no functionality)

---
+++ screens/Expenses.tsx
@@ -0,0 +1,3 @@
+import { Text, View } from 'react-native';
+
+export const Expenses = () => <Text>Hello, Expenses!</Text>;
---
+++ screens/Settings.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View } from 'react-native';
+import { ListItem } from '../Components/ListItem';
+import Entypo from '@expo/vector-icons/Entypo';
+import { theme } from '../theme';
+
+export const Settings = () => (
+  <View
+    style={{
+      margin: 16,
+      borderRadius: 11,
+      overflow: 'hidden',
+    }}
+  >
+    <ListItem
+      label="Categories"
+      detail={
+        <Entypo
+          name="chevron-thin-right"
+          color="white"
+          style={{ opacity: 0.3 }}
+          size={20}
+        />
+      }
+      onClick={() => {}}
+    />
+    <ListItem label="Erase all data" isDestructive onClick={() => {}} />
+  </View>
+);
